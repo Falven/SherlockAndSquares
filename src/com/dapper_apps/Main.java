be62ce6e2d5817df7d8e3c00861b6c768bbb8ae6
Francisco Aguilera <Falven2000@Hotmail.com>
2016-04-20 17:20:27 -0600

    Final commit.

--- src/com/dapper_apps/Main.java
+++ src/com/dapper_apps/Main.java
@@ -10,23 +10,25 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int t = Integer.parseInt(input.nextLine());
         String[] lines = new String[t];
-        while(t-- > 0) {
-            lines[t] = input.nextLine();
+        for(int i = 0; i < t; ++i) {
+            lines[i] = input.nextLine();
         }
-        for(int i = 0; i < lines.length; ++i) {
+        for(int i = 0; i < t; ++i) {
             String[] numbers = lines[i].trim().split("\\s+");
-            System.out.println(countSquares(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1])));
+            System.out.println(countSquares(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[numbers.length - 1])));
         }
     }
 
-    /**
-     * Count the number of square integers between a and b, inclusive.
-     * @param a
-     * @param b
-     * @return The number of square integers between a and b, inclusive.
-     */
     public static int countSquares(int a, int b) {
-        
-        return a + b;
+        // n =        3  4  5
+        // sqrt(n) = 1.7 2 2.4
+        //            2     2
+        //               0     + 1 = 2
+        // When you take the square root of the first and last number,
+        // all the numbers in between are the whole number square roots of the
+        // numbers in the non-square root range.
+        a = (int)Math.ceil(Math.sqrt(a));
+        b = (int)Math.floor(Math.sqrt(b));
+        return b - a + 1;
     }
 }
